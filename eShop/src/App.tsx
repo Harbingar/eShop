b4485ac128ec8f40a299522c6f4c2b784e8bd7e5
Thomas Syben <thomassyben07@gmail.com>
2024-01-24 14:29:27 +0100

    add TitleComponent

--- eShop/src/App.tsx
+++ eShop/src/App.tsx
@@ -1,12 +1,14 @@
 import './font.css';
 import Nav from "./component/navBar";
 import Footer from "./component/footer";
+import Welcome from "./component/welcome";
 
 function App() {
 
   return (
     <>
     <Nav/>
+    <Welcome/>
     <Footer/>
     </>
   )
---
+++ eShop/src/component/welcome.tsx
@@ -0,0 +1,16 @@
+import Title from "./welcomeTitle";
+const welcome: React.FC = () => {
+
+  return (
+    <div className="flex flex-row">
+        <div className="ml-4">
+           <Title/>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  );
+};
+
+export default welcome;
